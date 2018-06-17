619d5c6b8d752538f105d8ba85f4dc40d9688f29
U-DESKTOP-6OSFKN4\Deepak Subramanian <deepak0520@gmail.com>
2018-06-17 17:11:21 -0400

    This is almost final. Only thing left is style customization

--- Facebook Calendar Project/public/core.js
+++ Facebook Calendar Project/public/core.js
@@ -1,21 +1,121 @@
 (function(){
     var app = angular.module('IC', []);    
-    app.controller('EventC', function($http){
+    app.controller('EventC', function($http, $window, $sce){
         this.city = {};
+        this.showCityName = {};
         this.events = {};
+        this.Signed = 0;
+        this.find = 0;
         var self = this;
-        this.findEvents = function() {
-            console.log('finding events');
-            $http.post('/Events', this.city)
+        
+        this.checkSign = function(){
+            console.log('checking signIn on load');
+            $http.get('/checkSign')
+            .then(function(data){
+                if (data.data == 1){
+                    self.Signed = 1;
+                    console.log(self.Signed);                                                                
+                }
+                else if (data.data != 1){
+                    self.Signed = 0;
+                    console.log(self.Signed);                                                               
+                }
+            });
+        };
+
+        this.signIn = function() {
+            console.log ('signing in');
+            $http.post('/signIn')
+            .then(function(data){
+                self.url = data.data;
+                $window.open(self.url, "_self");
+            });
+        };
+
+        this.signOut = function(){
+            self.Signed = 0;
+            $http.get('/signOut')
+            .then(function(data){
+
+                console.log("redirect")
+            }); 
+            $window.open("http://localhost:4000/welcomeP", "_self");
+               
+        }
+
+        this.getLocation = function(){
+            if (navigator.geolocation) {
+                console.log("Finding Nearby Events")
+                navigator.geolocation.getCurrentPosition(self.showPosition);
+            } else { 
+                console.log("No Permission");
+            }
+        }
+
+        this.showPosition = function(position){
+            self.find = 1;        
+            temp = {text: "Current Location"}
+            self.showCityName = temp;
+            var location = {latitude: position.coords.latitude, longitude: position.coords.longitude};
+            $http.post('/findEventsLocation', location)
             .then(function(data){
                 self.events = {};
                 self.events = data.data.events;
-                console.log(self.events);
+                for (var i = 0; i < self.events.length; i++){
+                    self.events[i].index = i;
+                }
+            });
+        }
+
+        this.findEvents = function() {
+            self.find = 1;
+            self.showCityName = this.city;
+            console.log('Finding Events by City');
+            $http.post('/findEvents', this.city)
+            .then(function(data){
+                self.events = {};
+                self.events = data.data.events;
+                for (var i = 0; i < self.events.length; i++){
+                    self.events[i].index = i;
+                }
+                self.city = {};
             });   
         };
-        /*this.convertDate = function(){
-            return '1'
-        }*/
+
+        this.hideEvents = function(){
+            self.find = 0;
+            console.log('hiding events');
+            self.events = {}  
+        }
+
+        this.addNewEvents = function(inputIndex) {
+            console.log('adding new event');
+            var eventInfo = {'name': self.events[inputIndex].name.text, 
+            'description': self.events[inputIndex].url, 
+            'timeZone': self.events[inputIndex].start.timezone,
+            'start': self.events[inputIndex].start.local, 
+            'end':self.events[inputIndex].end.local};
+            $http.post('/addNew', eventInfo)
+            .then(function(data){
+                document.getElementById('myCal').src = document.getElementById('myCal').src;                console.log(eventInfo);
+            });
+        };
+
+        this.showEvents = function(){
+            console.log('show');
+            $http.get('/displayCalendar')
+            .then(function(data){
+                self.showCal = 1;
+                self.id = data.data;
+                self.id = self.id.replace('@', '%40');
+                self.calUrl = `https://calendar.google.com/calendar/embed?src=${self.id}&ctz=America%2FChicago`;
+                self.calUrl = $sce.trustAsResourceUrl(self.calUrl);
+            });
+        };
+
+        this.trustSrc = function(src) {
+            return $sce.trustAsResourceUrl(src);
+        }
 
         this.convertDate = function(start, end){
             var date1 = new Date(start);
--- Facebook Calendar Project/server.js
+++ Facebook Calendar Project/server.js
@@ -2,6 +2,16 @@ const request = require('request');
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+
+var {google} = require('googleapis');
+const ClientId = "928412217018-l5ckke4n65ghdv1ft2eh2rpk8gb133sb.apps.googleusercontent.com";
+const ClientSecret = "wFJd-BtcsIvQuSobkLqmEyrF";
+const RedirectionUrl = "http://localhost:4000/oauthCallback";
+
+var oauth2Client;
+var TokenTemp;
+var signedIn = 0;
+
 const app = express();
 
 app.use(bodyParser.urlencoded({
@@ -14,17 +24,44 @@ app.use(express.static(__dirname + '/public'));                 // set the stati
 
 app.set('view engine', 'ejs');
 
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname+'/public/mainCalendar.html'));
-    console.log('Main Path');
-})
-
 app.listen(4000, function () {
-   console.log('Example app listening on port 4000!')
+    console.log('Example app listening on port 4000!')
 })
 
+app.get('/', function (req, res) {
+    //res.sendFile(path.join(__dirname+'/public/mainCalendar.html'));
+    res.sendFile(path.join(__dirname+'/public/WelcomePage.html'));    
+    console.log('Welcome Page');
+})
 
-app.post('/Events', function (req, res) {
+app.use('/welcomeP', function (req, res) {
+    res.sendFile(path.join(__dirname+'/public/WelcomePage.html'));    
+    console.log('Welcome Page S');
+})
+
+app.get('/afterSign', function (req, res) {
+    res.sendFile(path.join(__dirname+'/public/mainCalendar.html'));
+    console.log('Main Page');
+})
+
+app.get('/checkSign', function(req,res){
+    console.log('checking signIn on load');
+    console.log(signedIn);
+    if (signedIn == 1){
+        res.send('1');
+    }
+    else if (signedIn != 1){
+        res.send('0');
+    }
+})
+
+app.post('/signIn', function(req, res){
+    console.log("Redirecting to Google Sign In");
+    var url = getAuthUrl();
+    res.send(url);
+})
+
+app.post('/findEvents', function (req, res) {
     let city = req.body.text; 
     console.log(city);  
     let url = `https://www.eventbriteapi.com/v3/events/search/?q=${city}&sort_by=date&token=H5ZSJRRYBV24CSNEURFI`;
@@ -34,12 +71,129 @@ app.post('/Events', function (req, res) {
             console.log('error:', err);
         }
         else{
-            //console.log(body);
+            console.log("Showing Events");
             let events = JSON.parse(body)
-            console.log(events.events[0].name.text);
-            console.log(events.events[0].category_id);
             res.send(events);
         }
     
     });    
-  })
+})
+
+app.post('/findEventsLocation', function (req, res) {
+    let latitude = req.body.latitude;
+    let longitude = req.body.longitude; 
+    console.log(latitude, longitude);  
+    let url = `https://www.eventbriteapi.com/v3/events/search/?sort_by=-date&location.within=10mi&location.latitude=${latitude}&location.longitude=${longitude}&token=H5ZSJRRYBV24CSNEURFI`;    
+    request(url, function (err, response, body) {
+        if (err){
+            console.log('error:', err);
+        }
+        else{
+            console.log("Showing Events");
+            let events = JSON.parse(body)
+            res.send(events);
+        }
+    });    
+})
+
+app.post('/addNew', function (req, res){
+    console.log("Adding New Event");        
+    addNewEvents(oauth2Client, req.body.name, req.body.description, req.body.timeZone, req.body.start, req.body.end);    
+    res.send('1');
+})
+
+app.get('/displayCalendar', function(req,res){
+  console.log("Displaying Events");        
+  listEvents(oauth2Client, function(ret){
+       res.send(ret);
+   });
+});
+
+//Added For testing
+function getOAuthClient() {
+    return new google.auth.OAuth2(ClientId, ClientSecret, RedirectionUrl);
+}
+
+function getAuthUrl() {
+    var oauth2Client = getOAuthClient();
+    var scopes = [
+        'https://www.googleapis.com/auth/calendar'
+    ];
+
+    var url = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: scopes,
+    });
+
+    return url;
+}
+
+app.use("/oauthCallback", function(req, res) {
+    oauth2Client = getOAuthClient();
+    var session = req.session;
+    var code = req.query.code;
+    oauth2Client.getToken(code, function(err, token) {
+        if (err){
+            signedIn = 0;
+            console.log("problem");
+            res.redirect("http://localhost:4000/");                           
+        }
+        else {
+            signedIn = 1;
+            oauth2Client.setCredentials(token);
+            TokenTemp = token;
+            res.redirect("http://localhost:4000/afterSign");               
+        }
+    });  
+});
+
+function listEvents(auth, cb) {
+    const calendar = google.calendar({version: 'v3', auth});
+   
+    calendar.calendars.get({
+        calendarId: 'primary',
+      }, (err, {data}) => {
+        if (err) {
+            console.log('The API returned an error: ' + err);
+            return err;
+        }
+        const info = data.id;
+        cb(info);
+      });
+      
+  }
+
+    function addNewEvents(auth, eventName, eventDescription, eventTimeZone, eventTimeStart, eventTimeEnd) {
+        var event = {
+            'summary': eventName,
+            'description': eventDescription,
+            'start': {
+              'dateTime': eventTimeStart,
+              'timeZone': 'America/Los_Angeles',
+            },
+            'end': {
+              'dateTime': eventTimeEnd,
+              'timeZone': 'America/Los_Angeles',
+            },
+            'reminders': {
+              'useDefault': false,
+              'overrides': [
+                {'method': 'email', 'minutes': 24 * 60},
+                {'method': 'popup', 'minutes': 10},
+              ],
+            },
+          };
+    
+          const calendar = google.calendar({version: 'v3', auth});  
+          calendar.events.insert({
+            auth: auth,
+            calendarId: 'primary',
+            resource: event,
+          }, function(err, event) {
+            if (err) {
+              console.log('There was an error contacting the Calendar service: ' + err);
+              return;
+            }
+            console.log('Event created: %s', event.summary);
+          });
+        }
